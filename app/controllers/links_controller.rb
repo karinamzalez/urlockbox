02b25590b43db921a24a5a3316162fef46a21848
Karina Gonzalez <karinamzalez@gmail.com>
2016-10-31 17:01:17 -0600

    go back to functional version

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,4 +3,22 @@ class LinksController < ApplicationController
     @user = current_user
     @links = Link.where(user_id: current_user)
   end
+
+  def create
+    @link = Link.new(link_params)
+    @link.user_id = current_user.id
+    if @link.save
+      flash[:success] = "Successfully created #{@link.title}"
+      render json: @link
+    else
+      flash.now[:warning] = @link.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  private
+
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,7 @@ Rails.application.routes.draw do
   root "homepage#show"
 
   resources :users, only: [:new, :create]
-
-  namespace :api do
-    namespace :v1, defaults: {format: :json}  do
-      post   "/links",       to: "links#create"
-    end
-  end
-  
-  resources :links, only: [:index]
+  resources :links, only: [:index, :create]
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
