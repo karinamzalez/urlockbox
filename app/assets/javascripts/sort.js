644a406220350434e21b553673eab8f86559d755
Karina Gonzalez <karinamzalez@gmail.com>
2016-11-09 12:01:55 -0700

    add SQL for alphabetical sort

--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
 
 var sortAlphabetically = function() {
   $('.alphabetical').on("click", function(){
-    
+    $.ajax({
+      method: "GET",
+      url: "/api/v1/links/sort-alphabetically",
+      success: function(links) {
+
+      }
+    });
   });
 };
