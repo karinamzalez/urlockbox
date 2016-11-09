f2ff3b55c181841807f56235e642787177009891
Karina Gonzalez <karinamzalez@gmail.com>
2016-11-08 18:49:32 -0700

    add line through when unread

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def render_status(status)
     status == "t" ? "true" : "false"
   end
+
+  def truncate_link(link)
+    limited_body = truncate(link, length: 20)
+  end
+
+  def check_status(status)
+    status == "t" ? "read" : "unread"
+  end
 end
