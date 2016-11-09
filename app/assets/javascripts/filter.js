887b68aa87554aee48c45e408085da5f99fc5088
Karina Gonzalez <karinamzalez@gmail.com>
2016-11-09 11:10:48 -0700

    add filter by url

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,18 +1,30 @@
 $(document).ready(function () {
   filterTitle();
+  filterUrl();
 });
 
 var filterTitle = function() {
   $('#link_filter_title').on("keyup", function(){
     var currentTitle = this.value;
-    var table = $('tbody')[0];
-    var links = table.children;
-    $.each(links, function (index, link){
-      if (link.getAttribute('data-title').indexOf(currentTitle) !== -1 ) {
-        link.style.display = "initial";
-      } else {
-        link.style.display = "none";
-      }
-    });
+    filter("data-title", currentTitle);
+  });
+};
+
+var filterUrl = function() {
+  $('#link_filter_url').on("keyup", function(){
+    var currentUrl = this.value;
+    filter("data-url", currentUrl);
+  });
+};
+
+var filter = function(data, currentQuery) {
+  var table = $('tbody')[0];
+  var links = table.children;
+  $.each(links, function (index, link){
+    if (link.getAttribute(data).indexOf(currentQuery) !== -1 ) {
+      link.style.display = "initial";
+    } else {
+      link.style.display = "none";
+    }
   });
 };
