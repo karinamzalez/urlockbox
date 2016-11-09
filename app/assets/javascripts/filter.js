5ce9502115ec44a3efb1426546532ad8dbac07b8
Karina Gonzalez <karinamzalez@gmail.com>
2016-11-09 09:46:24 -0700

    fix bug in edit link * change 'title_field' --> 'text_field'

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -3,12 +3,10 @@ $(document).ready(function () {
 });
 
 var filterTitle = function() {
-  var $links = $('.links');
   $('#link_filter_title').on("keyup", function(){
-    console.log(this.value);
     var currentTitle = this.value;
+    $links = $('.link');
     $links.each(function (index, link){
-      $link = $(link);
       if ($link.data('title').indexOf(currentTitle) !== -1 ) {
         $link.show();
       } else {
--- app/assets/javascripts/sort.js
+++ app/assets/javascripts/sort.js
@@ -4,20 +4,22 @@ $(document).ready(function(){
 });
 
 var onlyRead = function() {
-  $.ajax({
-    method: "GET",
-    url: "/api/v1/links/read-links",
-    data: {read: "t"},
-    dataType: "json",
-    success: function(links) {
-      var linksTable = $('.links')[0];
-      // debugger
-      while (linksTable.hasChildNodes()) {
-        linksTable.removeChild(linksTable.firstChild);
+  $(".read1").on("click", function(e) {
+    $.ajax({
+      method: "GET",
+      url: "/api/v1/links/read-links",
+      data: {read: "t"},
+      dataType: "json",
+      success: function(links) {
+        var linksTable = $('.links');
+        debugger
+        while (linksTable.hasChildNodes()) {
+          linksTable.removeChild(linksTable.firstChild);
+        }
+        // links.forEach(function(link) {
+        //   linksTable.appendChild("<div>")
+        // });
       }
-      // links.forEach(function(link) {
-      //   linksTable.appendChild("<div>")
-      // });
-    }
+    });
   });
 };
